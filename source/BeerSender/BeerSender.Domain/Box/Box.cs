3f8a117ddbca8f1d69cb03d87a3331400920ac11
Hannes Lowette <lowette.hannes@gmail.com>
2023-05-22 14:20:52 +0200

    Aggregate start

---
+++ source/BeerSender/BeerSender.Domain/Box/Box.cs
@@ -0,0 +1,25 @@
+﻿using BeerSender.Domain.Box.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerSender.Domain.Box
+{
+    internal class Box : Aggregate
+    {
+        public Box_size Box_size { get; private set; }
+        public bool Is_closed { get; private set; }
+
+        public void Apply(object @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Apply(Box_created @event)
+        {
+            Box_size = @event.Size;
+        }
+    }
+}
