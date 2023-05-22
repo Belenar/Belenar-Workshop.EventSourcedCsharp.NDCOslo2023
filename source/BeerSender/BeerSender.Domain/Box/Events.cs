a44883051aea556af4b55887d7198aa0772ec5ec
Alex Albu <alex.albu@novanet.no>
2023-05-22 14:22:25 +0200

    new event

--- source/BeerSender/BeerSender.Domain/Box/Events.cs
+++ source/BeerSender/BeerSender.Domain/Box/Events.cs
@@ -11,3 +11,5 @@ public record Box_failed_to_create(Box_size_exception.Fail_reason Reason) : Beer
 public record Shipping_label_added(Box_id Box_Id) : Beer_base_event;
 
 public record Box_closed(Box_id Box_Id) : Beer_base_event;
+
+public record Beer_added_to_box(Box_id Box_Id) : Beer_base_event;
