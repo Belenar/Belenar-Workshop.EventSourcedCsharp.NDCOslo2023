777890c56bd0bcd4320572aa8f2315d3291d1b65
Sverre Hundeide <github@hundeide.net>
2023-05-22 16:00:55 +0200

    Added test base classes and test for Select_box_size

---
+++ source/BeerSender/BeerSender.Tests/Box/Box_test.cs
@@ -0,0 +1,22 @@
+﻿using BeerSender.Domain.Box;
+using BeerSender.Domain.Box.Events;
+
+namespace BeerSender.Tests.Box;
+
+public class Box_test : Command_test
+{
+    private readonly Guid _aggregate_id = Guid.NewGuid();
+
+    //Commands
+    protected Domain.Box.Commands.Select_box_size Select_box_size_with_valid_number_of_bottles()
+    {
+        return new Domain.Box.Commands.Select_box_size(_aggregate_id, 24);
+    }
+
+
+    //Events
+    protected Box_created Box_is_created()
+    {
+        return new Box_created(Box_size.Create(24));
+    }
+}
---
+++ source/BeerSender/BeerSender.Tests/Box/Select_box_size_test.cs
@@ -0,0 +1,16 @@
+﻿namespace BeerSender.Tests.Box;
+
+public class Select_box_size_test : Box_test
+{
+    [Fact]
+    public void When_size_is_valid_returns_success()
+    {
+        // Given: Events from the past
+        // When: Command is executed
+        // Then: Specific events should occur
+
+        Given();
+        When(Select_box_size_with_valid_number_of_bottles());
+        Then(Box_is_created());
+    }
+}
