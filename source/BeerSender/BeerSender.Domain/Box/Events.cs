b9528bb0c2b5d2dd6ea9ff30404851fe0b23f533
Hannes Lowette <lowette.hannes@gmail.com>
2023-05-22 12:55:02 +0200

    Unsaved changes

--- source/BeerSender/BeerSender.Domain/Box/Events.cs
+++ source/BeerSender/BeerSender.Domain/Box/Events.cs
@@ -1,3 +1,7 @@
-﻿namespace BeerSender.Domain.Box.Events;
+﻿using BeerSender.Domain.Box.Exceptions;
+
+namespace BeerSender.Domain.Box.Events;
 
 public record Box_created(Box_size Size);
+
+public record Box_failed_to_create(Box_size_exception.Fail_reason Reason);
--- source/BeerSender/BeerSender.Domain/Box/Exceptions.cs
+++ source/BeerSender/BeerSender.Domain/Box/Exceptions.cs
@@ -1,6 +1,16 @@
-﻿namespace BeerSender.Domain.Box;
+﻿namespace BeerSender.Domain.Box.Exceptions;
 
-public class Exceptions
+public class Box_size_exception : Exception
 {
-    
+    private readonly Fail_reason _reason;
+
+    public Box_size_exception(Fail_reason reason)
+    {
+        _reason = reason;
+    }
+
+    public enum Fail_reason
+    {
+        Invalid_box_size
+    }
 }
