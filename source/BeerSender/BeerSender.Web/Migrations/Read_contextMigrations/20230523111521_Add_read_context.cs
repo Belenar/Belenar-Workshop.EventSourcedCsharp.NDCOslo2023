e76feb5b01d1860f7f677789a98d8cc4db581ad3
Sverre Hundeide <github@hundeide.net>
2023-05-23 13:17:07 +0200

    Added Read_context and read API

---
+++ source/BeerSender/BeerSender.Web/Migrations/Read_contextMigrations/20230523111521_Add_read_context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeerSender.Web.Migrations.Read_contextMigrations
+{
+    /// <inheritdoc />
+    public partial class Add_read_context : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ source/BeerSender/BeerSender.Web/ReadStore/Read_context.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BeerSender.Web.ReadStore;
+
+public class Read_context : DbContext
+{
+    public Read_context(DbContextOptions<Read_context> options) : base(options)
+    {
+    }
+
+    public DbSet<Box_status> Boxes => Set<Box_status>();
+    public DbSet<Box_bottle> Bottles => Set<Box_bottle>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Box_status>()
+            .HasKey(b => b.Aggregate_id);
+
+        modelBuilder.Entity<Box_bottle>()
+            .HasOne(bottle => bottle.Box)
+            .WithMany(box => box.Bottles)
+            .HasPrincipalKey(box => box.Aggregate_id)
+            .HasForeignKey(bottle => bottle.Box_id);
+    }
+}
+
+
+
+public class Box_status
+{
+    public Guid Aggregate_id { get; set; }
+    public int? Size { get; set; }
+    public ICollection<Box_bottle> Bottles { get; set; } = new HashSet<Box_bottle>();
+    public string? Carrier { get; set; }
+    public string? Tracking_code { get; set; }
+    public Box_state Status { get; set; } = Box_state.Created;
+}
+
+public class Box_bottle
+{
+    public int Id { get; set; }
+    public Guid Box_id { get; set; }
+    public Box_status Box { get; set; }
+    public string Brewery { get; set; }
+    public string Name { get; set; }
+    public decimal Percentage { get; set; }
+}
+
+public enum Box_state
+{
+    Created,
+    Closed,
+    Shipped
+}
